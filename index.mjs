fd7e22a6f89a5450ea3cb3f5954bf76f8123db7d
Dennis Kawurek <dennis.kawurek@hotmail.de>
2023-09-10 20:26:11 +0200

    Init data.

---
+++ index.mjs
@@ -0,0 +1,21 @@
+import { Client } from '@stomp/stompjs';
+
+import { WebSocket } from 'ws';
+Object.assign(global, { WebSocket });
+
+const client = new Client({
+  brokerURL: 'ws://localhost:5555/ws',
+  onConnect: () => {
+    console.log("connected!");
+    client.subscribe('/topic/1', message =>
+      console.log(JSON.parse(message.body))
+    );
+    setInterval(() => client.publish({
+      destination: '/topic/1',
+      body: JSON.stringify({ "message": "Hello, World!" }),
+      headers: {'content-type': 'application/json'}
+    }), 1000);
+  },
+});
+
+client.activate();
